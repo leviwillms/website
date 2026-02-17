fa8e21cc7ca5444158ef781ccec9cb19514829e2
levi-otu <levi.willms@ontariotechu.net>
2026-02-17 18:50:11 -0500

    Migrated the application to use Vite instead of CRA

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="vite/client" />
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.md' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.md?raw' {
+  const content: string;
+  export default content;
+}
